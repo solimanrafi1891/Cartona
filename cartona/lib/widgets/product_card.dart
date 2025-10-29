fa8ed21e38fb1e451b8d31756fbd2c269b09aaa2
solimanrafi1891 <154500185+solimanrafi1891@users.noreply.github.com>
2025-10-29 10:36:00 +0600

    widgets

---
+++ cartona/lib/widgets/product_card.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class ProductCard extends StatelessWidget {
+  final String imageUrl;
+  final String title;
+  final String price;
+
+  const ProductCard({
+    super.key,
+    required this.imageUrl,
+    required this.title,
+    required this.price,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+      elevation: 3,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Expanded(
+            child: ClipRRect(
+              borderRadius: const BorderRadius.vertical(top: Radius.circular(10)),
+              child: Image.network(imageUrl, fit: BoxFit.cover, width: double.infinity),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(title, style: const TextStyle(fontWeight: FontWeight.bold)),
+                const SizedBox(height: 5),
+                Text(price, style: const TextStyle(color: Colors.blueAccent)),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
